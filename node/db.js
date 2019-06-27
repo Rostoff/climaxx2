4a2aae2373d0d94292e664f7ca74dae26412417d
moi je <moi@je.com>
2019-06-27 15:45:41 +0200

    un texte

---
+++ node/db.js
@@ -0,0 +1,89 @@
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = 'mongodb://localhost:27017/ClimaxxDB';
+
+let dbConn;
+
+const connect = function(cb) {
+    MongoClient.connect(mongoURL, { useNewUrlParser: true }, (err, db) => {
+        if (err)
+            throw err;
+    
+        console.log('Connecté à MongoDb :)');
+    
+        dbConn = db;
+        cb();
+    })
+}
+
+let getVentilateur= function(cb){
+    if(!dbConn) {
+        connect(() => getVentilateur(cb));
+        return;
+    }
+    dbConn.db('ClimaxxDB').collection("Ventilateur").find({}).toArray((err, result)=>{
+        if (err){
+            console.log("err");
+            return;
+        }
+        cb(result);
+    });
+};
+let getClimatiseur= function(cb){
+    if(!dbConn) {
+        connect(() => getClimatiseur(cb));
+        return;
+    }
+    dbConn.db('ClimaxxDB').collection("Climatiseur").find({}).toArray((err, result)=>{
+        if (err){
+            console.log("err");
+            return;
+        }
+        cb(result);
+    });
+};
+let getClimatiseurEcolo= function(cb){
+    if(!dbConn) {
+        connect(() => getClimatiseurEcolo(cb));
+        return;
+    }
+    dbConn.db('ClimaxxDB').collection("ClimatiseurEcolo").find({}).toArray((err, result)=>{
+        if (err){
+            console.log("err");
+            return;
+        }
+        cb(result);
+    });
+};
+
+let getEventail= function(cb){
+    if(!dbConn) {
+        connect(() => getEventail(cb));
+        return;
+    }
+    dbConn.db('ClimaxxDB').collection("Eventail").find({}).toArray((err, result)=>{
+        if (err){
+            console.log("err");
+            return;
+        }
+        cb(result);
+    });
+};
+
+let getBrumatiseur= function(cb){
+    if(!dbConn) {
+        connect(() => getBrumatiseur(cb));
+        return;
+    }
+    dbConn.db('ClimaxxDB').collection("Brumatiseur").find({}).toArray((err, result)=>{
+        if (err){
+            console.log("err");
+            return;
+        }
+        cb(result);
+    });
+};
+
+
+module.exports = {
+    getBrumatiseur, getVentilateur, getEventail, getClimatiseur, getClimatiseurEcolo
+}
