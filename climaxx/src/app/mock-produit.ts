81c1284075794d67856393ede1714070925e664b
christophe <kitsuninja@hotmail.com>
2019-06-27 17:00:29 +0200

    produits page accueil

--- climaxx/src/app/mock-produit.ts
+++ climaxx/src/app/mock-produit.ts
@@ -1,14 +1,14 @@
 import { Produit } from './produit';
 
 export const PRODUITS: Produit[]= [
-    {id: 1, name: 'Ventilo turbine', descriptif: 'C\'est génialement frais', prix: 99, quantity: 150},
-    {id: 1, name: 'Brumisateur gelé', descriptif: 'C\'est génialement froid', prix: 49, quantity: 150},
-    {id: 1, name: 'Climatisation Chofrois', descriptif: 'C\'est génialement frisé', prix: 199, quantity: 150},
-    {id: 1, name: 'Eventail du futur', descriptif: 'C\'est génialement frais', prix: 9, quantity: 150},
-    {id: 1, name: 'Ventilation auto', descriptif: 'C\'est frais', prix: 49, quantity: 150},
-    {id: 1, name: 'climatisation chauffo', descriptif: 'C\'est cool', prix: 79, quantity: 150},
-    {id: 1, name: 'Ventilo ecolo', descriptif: 'C\'est pas mal en été', prix: 19, quantity: 150},
-    {id: 1, name: 'Ventilo vitesse lapin', descriptif: 'C\'est fraichement frais', prix: 29, quantity: 150},
-    {id: 1, name: 'Ventilopio', descriptif: 'C\'est ventilo-centré', prix: 299, quantity: 150},
-    {id: 1, name: 'Cimatosceptic', descriptif: 'C\'est faux !!', prix: 5, quantity: 150}
+    {id: 1, name: 'Ventilo turbine', descriptif: 'C\'est génialement frais', prix: 99, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Brumisateur gelé', descriptif: 'C\'est génialement froid', prix: 49, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Climatisation Chofrois', descriptif: 'C\'est génialement frisé', prix: 199, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Eventail du futur', descriptif: 'C\'est génialement frais', prix: 9, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Ventilation auto', descriptif: 'C\'est frais', prix: 49, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'climatisation chauffo', descriptif: 'C\'est cool', prix: 79, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Ventilo ecolo', descriptif: 'C\'est pas mal en été', prix: 19, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Ventilo vitesse lapin', descriptif: 'C\'est fraichement frais', prix: 29, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Ventilopio', descriptif: 'C\'est ventilo-centré', prix: 299, quantity: 150, img:'assets/image/ventiloplage2.jpg'},
+    {id: 1, name: 'Cimatosceptic', descriptif: 'C\'est faux !!', prix: 5, quantity: 150, img:'assets/image/ventiloplage2.jpg'}
 ]
--- climaxx/src/app/produit.ts
+++ climaxx/src/app/produit.ts
@@ -4,4 +4,5 @@ export class Produit {
     descriptif: string;
     prix: number;
     quantity: number;
+    img: string;
 }
